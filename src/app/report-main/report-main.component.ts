7b1a598382bdf36420477bfe6416d1d5395abe20
dilip <dilip.inexture@gmail.com>
2018-10-01 18:49:55 +0530

    report table design

--- src/app/report-main/report-main.component.ts
+++ src/app/report-main/report-main.component.ts
@@ -11,9 +11,43 @@ export class ReportMainComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+	$(document).ready(function() {
+	  function setHeight() {
+	    let windowHeight = $(window).innerHeight();
+	    let reportMenu = $('.report_menu_section').height();
+	    let headerView = $('.header_view').height();
+	    
+	    let removeHeight = windowHeight - reportMenu - headerView - 42;
 
-  	
-  	
+	    $('.container-fluid').css("height", removeHeight);
+
+	     //console.log(menuIcon, userProfile, removeHeight, windowHeight);
+	  };
+	  setHeight();
+	  
+	  $(window).resize(function() {
+	    setHeight();
+	  });
+	});
+
+	$(document).ready(function() {
+	  function setHeight1() {
+	    let windowHeight = $(window).innerHeight();
+	    let reportMenu = $('.report_menu_section').height();
+	    let headerView = $('.header_view').height();
+	    let rightHeaderView = $('.right_header_view').height();
+	    
+	    let removeHeight = windowHeight - reportMenu - headerView - rightHeaderView - 60;
+
+	    $('.report_table_view').css("height", removeHeight);
+
+	     //console.log(menuIcon, userProfile, removeHeight, windowHeight);
+	  };
+	  setHeight1();
+	  
+	  $(window).resize(function() {
+	    setHeight1();
+	  });
+	});
   }
-
 }
