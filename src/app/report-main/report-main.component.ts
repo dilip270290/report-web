2a9ded734cd1e161efd68daa8c3eab05bf931f05
dilip <dilip.inexture@gmail.com>
2018-10-10 18:24:39 +0530

    date picker added

--- src/app/report-main/report-main.component.ts
+++ src/app/report-main/report-main.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, HostListener, ElementRef } from '@angular/core';
 import * as $ from 'jquery';
+declare var moment: any;
 
 @Component({
   selector: 'app-report-main',
@@ -8,38 +9,35 @@ import * as $ from 'jquery';
 })
 export class ReportMainComponent implements OnInit {
 	
-	@HostListener('document:click', ['$event'])
-	clickout(event: any) {
-		if (this.eRef.nativeElement.contains(event.target)) {
-			console.log(event.srcElement.className);
-			// if (event.srcElement.className == '') {
-			// 	this.isShow = false;	
-			// 	this.isProduct = false;
-			// 	this.isContains = false;
-			// } 
-			// if (event.srcElement.className !== 'pro_drop_list') {
-			// 	this.isShow = false;
-			// }
-			if (event.srcElement.className === 'pro_drop_list ch_rep') {
-				this.isProduct = false;
-				this.isContains = false;
-			} else if (event.srcElement.className === 'pro_drop_list') {
-				this.isShow = false;
-				this.isProduct = false;
-			} else if (event.srcElement.className === 'pro_drop_list pro_li_drop') {
-				this.isContains = false;
-				this.isShow = false;
-			} else {
-				this.isShow = false;	
-				this.isProduct = false;
-				this.isContains = false;
-			}
-		}else{
-			this.isShow = false;	
-			this.isProduct = false;
-			this.isContains = false;
-		}
-	}
+	// @HostListener('document:click', ['$event'])
+	// clickout(event: any) {
+	// 	if (this.eRef.nativeElement.contains(event.target)) {
+	// 		console.log(event.srcElement.className);
+			
+	// 		if (event.srcElement.className === 'pro_drop_list ch_rep') {
+	// 			this.isProduct = false;
+	// 			this.isContains = false;
+	// 		} else if (event.srcElement.className === 'pro_drop_list') {
+	// 			this.isShow = false;
+	// 			this.isProduct = false;
+	// 		} else if (event.srcElement.className === 'pro_drop_list pro_li_drop') {
+	// 			this.isContains = false;
+	// 			this.isShow = false;
+	// 		} else if (event.srcElement.className === 'test') {
+	// 			this.isShow = false;	
+	// 			this.isProduct = false;
+	// 			this.isContains = false;
+	// 		} else {
+	// 			this.isShow = false;	
+	// 			this.isProduct = false;
+	// 			this.isContains = false;
+	// 		}
+	// 	}else{
+	// 		this.isShow = false;	
+	// 		this.isProduct = false;
+	// 		this.isContains = false;
+	// 	}
+	// }
 
   isShow = false;	
   isProduct = false;
@@ -84,7 +82,38 @@ export class ReportMainComponent implements OnInit {
 	  $(window).resize(function() {
 	    setHeight1();
 	  });
+
+
+
 	});
+
+	// $(document).ready(function() {
+
+ //      var cb = function(start, end, label) {
+ //        $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+ //      }
+ //      var ranges = {
+ //        'Today': [moment(), moment()],
+ //        'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+ //        'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+ //        'Last 14 Days': [moment().subtract(13, 'days'), moment()],
+ //        'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+ //        'This Month': [moment().startOf('month'), moment().endOf('month')],
+ //        'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+ //      };
+ //      var optionSet3 = {
+ //        startDate: moment().subtract(16, 'days'),
+ //        endDate: moment(),
+ //        opens: 'left',
+ //        showRangeInputsOnCustomRangeOnly: true,
+ //        ranges: ranges
+ //      };
+
+ //      $('#reportrange span').html(moment().subtract(29, 'days').format('MMMM D, YYYY') + ' - ' + moment().format('MMMM D, YYYY'));
+ //      $('#reportrange').daterangepicker(optionSet3, cb);
+ //      $('#destroy').click(function() {$('#reportrange').data('daterangepicker').remove();});
+ //   });
+
   }
 
   showFilters() {
